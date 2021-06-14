6964cf7768eaf53f8428b6e94c75c239139275ae
Bernd68452 <47543672+Bernd68452@users.noreply.github.com>
2021-06-14 20:16:58 +0200

    Fin

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Firstsection from './components/Firstsection';
+import Head from './components/Head';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="what">
+      <Head/>
+      <Firstsection/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
